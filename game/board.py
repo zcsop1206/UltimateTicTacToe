9e69233c86d89846c7b65983e56ba5e00fc03ff7
zcsop1206 <115398989+zcsop1206@users.noreply.github.com>
2025-02-26 00:17:57 -0500

    Update board.py

--- game/board.py
+++ game/board.py
@@ -2,4 +2,4 @@ def print_board(board):
     for i in range(0, 9, 3):
         print(" | ".join([str(board[j]) if board[j] != 0 else " " for j in range(i, i+3)]))
         if i < 6:
-            print("-" * 5)
+            print("-" * 10)
